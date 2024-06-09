a7c2f3cfba4fb50dd1720f6815867c206783f8ca
udaynarwal72 <udaynarwal72@gmail.com>
2024-06-09 16:52:20 +0530

    front setup

--- apps/Website-Frontend/src/main.jsx
+++ apps/Website-Frontend/src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+import App from './pages/App';
+import './index.css';
+import HeaderProvider from './context/HeaderProvider';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <HeaderProvider>
+    <RouterProvider router={router} />
+  </HeaderProvider>,
+);
