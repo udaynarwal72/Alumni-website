6bed043fded7dc9e4525e07e820c4566c03613fe
udaynarwal72 <udaynarwal72@gmail.com>
2024-06-10 18:00:04 +0530

    update

--- apps/Website-Frontend/src/components/signup/index.jsx
+++ apps/Website-Frontend/src/components/signup/index.jsx
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import './signup.css';
 import Footer from '../footer';
-import NavBar from '../Navbar'
+import NavBar from '../Navbar';
+import dataCountry from '../../../../../src/countries.json';
+import dataState from '../../../../../src/states.json';
 
-const branches = ['Computer Science', 'Electronics and Communication', 'Information Technology', 'Mechanical', 'Electrical', 'Civil', 'Production and Industrial Engineering', 'Mathematics and Computing', 'Industrial Internet of Things', 'Other'];
+const branches = [
+    'Computer Science', 'Electronics and Communication', 'Information Technology',
+    'Mechanical', 'Electrical', 'Civil', 'Production and Industrial Engineering',
+    'Mathematics and Computing', 'Industrial Internet of Things', 'Other'
+];
 
 function SignupPage() {
     const [selectedCountry, setSelectedCountry] = useState('');
@@ -12,69 +18,45 @@ function SignupPage() {
     const [cities, setCities] = useState([]);
     const [fetchedCountries, setFetchedCountries] = useState([]);
 
-    const countriesData = [
-        "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda",
-        "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas",
-        "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin",
-        "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei",
-        "Bulgaria", "Burkina Faso", "Burundi", "Cabo Verde", "Cambodia", "Cameroon",
-        "Canada", "Central African Republic", "Chad", "Chile", "China", "Colombia",
-        "Comoros", "Congo, Democratic Republic of the", "Congo, Republic of the",
-        "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti",
-        "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea",
-        "Eritrea", "Estonia", "Eswatini", "Ethiopia", "Fiji", "Finland", "France", "Gabon",
-        "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea",
-        "Guinea-Bissau", "Guyana", "Haiti", "Honduras", "Hungary", "Iceland", "India",
-        "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan",
-        "Kazakhstan", "Kenya", "Kiribati", "Korea, North", "Korea, South", "Kosovo", "Kuwait",
-        "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein",
-        "Lithuania", "Luxembourg", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali",
-        "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia",
-        "Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar",
-        "Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger",
-        "Nigeria", "North Macedonia", "Norway", "Oman", "Pakistan", "Palau", "Palestine",
-        "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal",
-        "Qatar", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia",
-        "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe",
-        "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore",
-        "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Sudan",
-        "Spain", "Sri Lanka", "Sudan", "Suriname", "Sweden", "Switzerland", "Syria", "Taiwan",
-        "Tajikistan", "Tanzania", "Thailand", "Timor-Leste", "Togo", "Tonga", "Trinidad and Tobago",
-        "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates",
-        "United Kingdom", "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City",
-        "Venezuela", "Vietnam", "Yemen", "Zambia", "Zimbabwe"
-    ];
-
     useEffect(() => {
-        setFetchedCountries(countriesData);
+        const userCountries = dataCountry.map(country => `${country.name}+${country.id}+${country.iso2}`);
+        setFetchedCountries(userCountries);
     }, []);
 
     useEffect(() => {
         if (selectedCountry) {
-            setSelectedState('');
+            const selectedCountryId = selectedCountry.split('+')[1];
+            const userStates = dataState.filter(state => state.country_id == selectedCountryId);
+            setStates(userStates.map(state => `${state.name}+${state.id}+${state.iso2}`));
+        } else {
+            setStates([]);
             setCities([]);
-
-            const fetchStateAndCities = async () => {
-                try {
-                    const response = await fetch(
-                        `https://ccsapi.up.railway.app/api/v1/search-db?search=${selectedCountry}&limit=10&offSet=10`
-                    );
-                    const data = await response.json();
-                    console.log(data);
-                    if (data.success && data.results.length > 0) {
-                        const stateAndCity = data.results[0].csc.split(', ');
-                        setStates(stateAndCity[1]); // Assuming the state is the second element
-                        setCities([stateAndCity[0]]); // Assuming the city is the first element
-                    }
-                } catch (error) {
-                    console.error("Error fetching state and cities:", error);
-                }
-            };
-
-            fetchStateAndCities();
         }
     }, [selectedCountry]);
 
+    useEffect(() => {
+        if (selectedState) {
+            const selectedCountryIso2 = selectedCountry.split('+')[2];
+            const selectedStateIso2 = selectedState.split('+')[2];
+            var headers = new Headers();
+            headers.append("X-CSCAPI-KEY", "UEFXY210QzFpUzlLdlpUTUlBelNrNjVNcUJwS2xYSjUzMGlMY3UxTg==");
+
+            var requestOptions = {
+                method: 'GET',
+                headers: headers,
+                redirect: 'follow'
+            };
+
+            fetch(`https://api.countrystatecity.in/v1/countries/${selectedCountryIso2}/states/${selectedStateIso2}/cities`, requestOptions)
+                .then(response => response.json())
+                .then(result => {
+                    setCities(result.map(city => city.name));
+                })
+                .catch(error => console.log('error', error));
+        } else {
+            setCities([]);
+        }
+    }, [selectedState, selectedCountry]);
 
     return (
         <>
@@ -102,8 +84,8 @@ function SignupPage() {
                             <label htmlFor="country">Country</label>
                             <select id="country" name="country" value={selectedCountry} onChange={(e) => setSelectedCountry(e.target.value)} required>
                                 <option value="">Joining Country</option>
-                                {fetchedCountries.map((country) => (
-                                    <option key={country} value={country}>{country}</option>
+                                {fetchedCountries.map(country => (
+                                    <option key={country} value={country}>{country.split('+')[0]}</option>
                                 ))}
                             </select>
                         </div>
@@ -111,8 +93,8 @@ function SignupPage() {
                             <label htmlFor="state">State</label>
                             <select id="state" name="state" value={selectedState} onChange={(e) => setSelectedState(e.target.value)} required>
                                 <option value="">Joining State</option>
-                                {states.map((state) => (
-                                    <option key={state} value={state}>{state}</option>
+                                {states.map(state => (
+                                    <option key={state} value={state}>{state.split('+')[0]}</option>
                                 ))}
                             </select>
                         </div>
@@ -120,7 +102,7 @@ function SignupPage() {
                             <label htmlFor="city">City</label>
                             <select id="city" name="city" required>
                                 <option value="">Joining City</option>
-                                {cities.map((city) => (
+                                {cities.map(city => (
                                     <option key={city} value={city}>{city}</option>
                                 ))}
                             </select>
@@ -133,7 +115,7 @@ function SignupPage() {
                             <label htmlFor="branch">Branch</label>
                             <select id="branch" name="branch" required>
                                 <option value="">Select Branch</option>
-                                {branches.map((branch) => (
+                                {branches.map(branch => (
                                     <option key={branch} value={branch}>{branch}</option>
                                 ))}
                             </select>
